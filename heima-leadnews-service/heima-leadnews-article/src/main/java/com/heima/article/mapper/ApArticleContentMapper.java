52c2150f599286056075a2bf5f166677f39061a5
嘉然今天吃向晚 <1483823709@qq.com>
2024-12-26 02:36:56 +0800

    文章内容页，文件上传和文章内容静态页面渲染集成

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/mapper/ApArticleContentMapper.java
@@ -0,0 +1,14 @@
+package com.heima.article.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.heima.model.article.pojos.ApArticleContent;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Description
+ * @Author 嘉然今天吃向晚
+ * @Date 2024/12/26-00:52:33
+ */
+@Mapper
+public interface ApArticleContentMapper extends BaseMapper<ApArticleContent> {
+}
---
+++ heima-leadnews-service/heima-leadnews-article/src/test/java/ArticleFreemarkerTest.java
@@ -0,0 +1,73 @@
+import com.alibaba.fastjson.JSONArray;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.heima.article.ArticleApplication;
+import com.heima.article.mapper.ApArticleContentMapper;
+import com.heima.article.service.ApArticleService;
+import com.heima.file.service.FileStorageService;
+import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.article.pojos.ApArticleContent;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @Description
+ * @Author 嘉然今天吃向晚
+ * @Date 2024/12/26-00:49:30
+ */
+@SpringBootTest(classes = ArticleApplication.class)
+@RunWith(SpringRunner.class)
+public class ArticleFreemarkerTest {
+
+    @Autowired
+    private ApArticleContentMapper apArticleContentMapper;
+
+    @Autowired
+    private Configuration configuration;
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @Autowired
+    private ApArticleService apArticleService;
+
+    @Test
+    public void test() throws IOException, TemplateException {
+        // 1. 获取文章内容
+        ApArticleContent content = apArticleContentMapper.selectOne(
+                Wrappers.<ApArticleContent>lambdaQuery()
+                        .eq(ApArticleContent::getArticleId, "1383827995813531650L"));
+        if (StringUtils.isNotBlank(content.getContent())) {
+            // 2. 文章内容通过freemarker自动生成html文件
+            Template template = configuration.getTemplate("article.ftl");
+            // 数据模型
+            Map<String, Object> cont = new HashMap<>();
+            cont.put("content", JSONArray.parseArray(content.getContent()));
+            StringWriter out = new StringWriter();
+            // 合成
+            template.process(cont, out);
+            // 3. 把html上传到minio中
+            InputStream in = new ByteArrayInputStream(out.toString().getBytes());
+            String path = fileStorageService.uploadHtmlFile("", content.getArticleId() + ".html", in);
+            // 4. 修改ap_article表，保存static_url字段
+            apArticleService.update(Wrappers.<ApArticle>lambdaUpdate()
+                    .eq(ApArticle::getId, content.getArticleId())
+                    .set(ApArticle::getStaticUrl, path));
+
+        }
+    }
+}
