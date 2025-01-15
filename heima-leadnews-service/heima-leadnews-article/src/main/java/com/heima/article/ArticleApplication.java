5d3d211bfa13b8c85311d9556919a068c60d3252
ZhijiangDiana <1483823709@qq.com>
2025-01-15 23:25:24 +0800

    修改启动类使所有微服务都能使用异步方法、远程调用业务

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/ArticleApplication.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/ArticleApplication.java
@@ -7,6 +7,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -14,6 +15,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.heima.article.mapper")
+@EnableFeignClients("com.heima.apis")
 @EnableAsync
 public class ArticleApplication {
 
--- heima-leadnews-service/heima-leadnews-schedule/src/main/java/com/heima/schedule/ScheduleApplication.java
+++ heima-leadnews-service/heima-leadnews-schedule/src/main/java/com/heima/schedule/ScheduleApplication.java
@@ -6,7 +6,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
@@ -14,6 +16,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableDiscoveryClient
 @MapperScan("com.heima.schedule.mapper")
 @EnableScheduling  // 开启调度任务
+@EnableFeignClients("com.heima.apis")
+@EnableAsync
 public class ScheduleApplication {
 
     public static void main(String[] args) {
