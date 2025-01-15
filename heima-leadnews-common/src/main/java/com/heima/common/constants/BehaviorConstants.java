2de6c1473293d48cd95c9ae9502bb897a66d40f9
ZhijiangDiana <1483823709@qq.com>
2025-01-16 00:29:25 +0800

    使用户行为数据的key符合规范

--- heima-leadnews-common/src/main/java/com/heima/common/constants/BehaviorConstants.java
+++ heima-leadnews-common/src/main/java/com/heima/common/constants/BehaviorConstants.java
@@ -2,16 +2,16 @@ package com.heima.common.constants;
 
 public class BehaviorConstants {
 
-    public static final String ARTICLE_LIKE = "article_like_";
-    public static final String MOMENT_LIKE = "moment_like_";
-    public static final String COMMENT_LIKE = "comment_like_";
+    public static final String ARTICLE_LIKE = "article_like:";
+    public static final String MOMENT_LIKE = "moment_like:";
+    public static final String COMMENT_LIKE = "comment_like:";
 
     public static final Short ARTICLE_LIKE_CODE = 0;
     public static final Short MOMENT_LIKE_CODE = 1;
     public static final Short COMMENT_LIKE_CODE = 2;
 
-    public static final String ARTICLE_READ = "article_read_";
-    public static final String ARTICLE_DISLIKE = "article_dislike_";
+    public static final String ARTICLE_READ = "article_read:";
+    public static final String ARTICLE_DISLIKE = "article_dislike:";
 
     public static final Short LIKE_OPERATION = 0;
     public static final Short DISCARD_LIKE_OPERATION = 1;
