44d0b7827b6070676f94b225e39304f732f44426
嘉然今天吃向晚 <1483823709@qq.com>
2025-05-10 17:07:25 +0800

    热文章分值计算完善

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/job/ComputeHotArticleJob.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/job/ComputeHotArticleJob.java
@@ -13,10 +13,17 @@ public class ComputeHotArticleJob {
     @Autowired
     private HotArticleService hotArticleService;
 
+    @XxlJob("syncArticleInfo")
+    public void syncInfo() {
+        log.info(">>>>>>>>>>> 开始同步文章信息");
+        hotArticleService.syncArticleInfo();
+        log.info(">>>>>>>>>>> 同步文章信息完成");
+    }
+
     @XxlJob("computeHotArticleJob")
     public void handle() {
-        log.info(">>>>>>>热文章分值计算调度任务执行");
+        log.info(">>>>>>>>>>> 热文章分值计算调度任务执行");
         hotArticleService.computeHotArticle();
-        log.info(">>>>>>>热文章分值计算调度任务执行完成");
+        log.info(">>>>>>>>>>> 热文章分值计算调度任务执行完成");
     }
 }
